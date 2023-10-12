548f413b8327f35a5ecd675987f96f6759bd1909
Caesy <Caesy@CAESY-DESKTOP>
2023-10-12 18:24:32 -0500

    Add project files.

---
+++ BookStore/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using BookStore.Models;
+
+namespace BookStore.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<BookStore.Models.Book> Book { get; set; } = default!;
+    }
+}
